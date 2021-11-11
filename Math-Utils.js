6ce740f8f0516b7cfaf3c6a097bfc9dd6ffa4c5a
shimapi <shirley.mallea2@gmail.com>
2021-11-11 20:28:11 -0300

    Se agrego la funcion EsMayor para poder comparar entre 2 numeros

--- Math-Utils.js
+++ Math-Utils.js
@@ -4,4 +4,15 @@ function suma(a, b) {
 	return a + b
 }
 
-suma(12, 22)
+// suma(12, 22)
+
+//Funcion es Mayor
+function esMayor(a, b) {
+	if (a > b) {
+		console.log(a + ' es mayor que ' + b)
+	} else {
+		console.log(b + ' es mayor que ' + a)
+	}
+}
+
+esMayor(44, 22)
