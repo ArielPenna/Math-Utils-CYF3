bfd0fa1327d0d314e7862a2b2da2b46bccfe3172
ArielPenna <arielpenna@gmail.com>
2021-11-16 18:45:28 -0300

    se agrego raizCuadrada

--- Math-Utils.js
+++ Math-Utils.js
@@ -28,10 +28,13 @@ function esMenor(a, b) {
 
 esMenor(33, 45)
 
+console.log('Cintya')
+console.log('Lishi')
+console.log('maca')
+console.log('Caro')
+console.log('Holiwis equipo azul')
+console.log('esto se ve entrete y prometedor! Vamos!')
 
-console.log("Cintya");
-console.log("Lishi");
-console.log("maca")
-console.log("Caro");
-console.log("Holiwis equipo azul");
-console.log("esto se ve entrete y prometedor! Vamos!");
+function raizCuadrada(numero) {
+	return Math.sqrt(numero)
+}
