563a4a38c63051fabce7171ca7a9c4490bbd8ec3
ArielPenna <arielpenna@gmail.com>
2021-11-16 18:54:46 -0300

    prueba

--- Math-Utils.js
+++ Math-Utils.js
@@ -28,10 +28,10 @@ function esMenor(a, b) {
 
 esMenor(33, 45)
 
-
-console.log("Cintya");
-console.log("Lishi");
-console.log("maca")
-console.log("Caro");
-console.log("Holiwis equipo azul");
-console.log("esto se ve entrete y prometedor! Vamos!");
+console.log('Cintya')
+console.log('Lishi')
+console.log('maca')
+console.log('Caro')
+console.log('Holiwis equipo azul')
+console.log('esto se ve entrete y prometedor! Vamos!')
+console.log('Prueba!!!!')
