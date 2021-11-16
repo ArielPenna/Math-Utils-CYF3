310a532b35b7003f89826eec7c62f8ec995da9db
macabrutal <maca@brutalrocks.com>
2021-11-16 20:04:45 -0300

    delete console.log

--- Math-Utils.js
+++ Math-Utils.js
@@ -28,29 +28,10 @@ function esMenor(a, b) {
 
 esMenor(33, 45)
 
-<<<<<<< HEAD
-console.log("maca")
-console.log("Caro");
-console.log("Holiwis equipo azul");
-<<<<<<< HEAD
-=======
-console.log("esto se ve entrete y prometedor! Vamos!");
-console.log('Cintya')
-console.log('Lishi')
-console.log('maca')
-console.log('Caro')
-console.log('Holiwis equipo azul')
-console.log('esto se ve entrete y prometedor! Vamos!')
-console.log('Prueba!!!!')
->>>>>>> d3383b4c68725f82db3d3cce91d95f9226c07fca
-=======
 
-console.log("Cintya");
-console.log("Lishi");
-console.log("maca")
 
 //raizCuadrada
 function raizCuadrada(numero){
 return Math.sqrt(numero)
 }
->>>>>>> funcion-raizCuadrada
+
